a2b369597317dfecd6c3b487fcda7e61b45e4303
binhlecong <congbinh21012001@gmail.com>
2021-12-21 21:48:45 +0700

    feat: push data to firebase by user input

--- app/src/main/java/com/binhlc/firebasedemo/MainActivity.java
+++ app/src/main/java/com/binhlc/firebasedemo/MainActivity.java
@@ -2,7 +2,9 @@ package com.binhlc.firebasedemo;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +12,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 
-import java.util.HashMap;
-
 public class MainActivity extends AppCompatActivity {
     private Button btnLogout;
+    private Button btnSubmit;
+    private EditText etInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnLogout = findViewById(R.id.btn_logout);
+        btnSubmit = findViewById(R.id.btn_submit);
+        etInput = findViewById(R.id.et_input);
 
         btnLogout.setOnClickListener(v -> {
             FirebaseAuth.getInstance().signOut();
@@ -31,11 +35,24 @@ public class MainActivity extends AppCompatActivity {
 
 //        FirebaseDatabase.getInstance().getReference().child("Binh's Firebase demo").child(
 //                "Android").setValue("Today is 21st of December");
-        HashMap<String, Object> data = new HashMap<>();
-        data.put("Name", "Le Cong Binh");
-        data.put("Birthday", "21/01/2001");
-
-        FirebaseDatabase.getInstance().getReference().child("Multiple value").child("Binh's Firebase demo")
-                .updateChildren(data);
+//        HashMap<String, Object> data = new HashMap<>();
+//        data.put("Name", "Le Cong Binh");
+//        data.put("Birthday", "21/01/2001");
+//
+//        FirebaseDatabase.getInstance().getReference().child("Multiple value").child("Binh's " +
+//                "Firebase demo")
+//                .updateChildren(data);
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputText = etInput.getText().toString();
+                if (inputText.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Empty input", Toast.LENGTH_SHORT);
+                } else {
+                    FirebaseDatabase.getInstance().getReference()
+                            .child("FirebaseDemo").push().child("Name").setValue(inputText);
+                }
+            }
+        });
     }
 }
